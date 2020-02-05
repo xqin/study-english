729821eae03cbc6f6955388d05aa0704134aa1a2
xqin <dds_feng@qq.com>
2020-02-05 18:22:04 +0800

    缓存 单词查询 结果

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const http = require('request').defaults({
 })
 const express = require('express')
 const app = express()
+const crypto = require('crypto')
+const fs = require('fs')
+
+const md5 = (str) => {
+  const hash = crypto.createHash('md5')
+  return hash.update(str, 'binary').digest('hex')
+}
 
 /**
  * 从指定的数据中， 获取 正则表达式 的所有可匹配到的结果
@@ -30,48 +37,75 @@ const getRegValue = (reg, data) => {
   }
 }
 
+const cache_file = './cache.json'
+const cache = {}
+let t = null
+
+const save2Cache = () => {
+  clearTimeout(t)
+  t = setTimeout(() => {
+    fs.writeFileSync(cache_file, JSON.stringify(cache, 0, 2))
+  }, 500)
+}
+
+try { Object.assign(cache, require(cache_file)) } catch (e) {}
+
 const get = (url) => new Promise((resolve, reject) => http.get(url, (e, res, body) => e ? reject(e) : resolve(body)))
-const dict = (word) => get(`http://dict.cn/${encodeURIComponent(word)}`).then((html) => {
-  html = html.replace(/<script[\s\S]+?<\/script>/g, '')
+const dict = (word) => {
+  const key = md5(word)
 
-  let symbol, exp, audios
-
-  const phonetic = getRegValue(/<div class="phonetic">([\s\S]+?)<\/div>/, html)
-
-  if (phonetic) {
-    // 英 <bdo>['dɪkt]</bdo>
-    symbol = getRegValues(/<span>(.\s+<bdo lang="EN-US">.+?<\/bdo>)/g, phonetic)
-
-    if (symbol) {
-      symbol = symbol.map((v) => v.replace(/^(.)\s+/, '$1'))
-    }
-
-    audios = getRegValues(/naudio="(.+?)" title/g, phonetic)
-
-    if (audios) {
-      audios = audios.map((audioId) => `/audio/${audioId}`)
-    }
+  if (typeof cache[key] !== 'undefined') {
+    return Promise.resolve(cache[key])
   }
 
-  let dict = getRegValue(/<ul class="dict-basic-ul">([\s\S]+?)<\/ul>/, html)
+  return get(`http://dict.cn/${encodeURIComponent(word)}`).then((html) => {
+    html = html.replace(/<script[\s\S]+?<\/script>/g, '')
 
-  if (dict) {
-    exp = getRegValues(/<li>\s+((?:<span>.+?<\/span>\s+)?<strong>.+?<\/strong>)\s+<\/li>/g, dict)
+    let symbol, exp, audios
+
+    const phonetic = getRegValue(/<div class="phonetic">([\s\S]+?)<\/div>/, html)
+
+    if (phonetic) {
+      symbol = getRegValues(/<span>(.\s+<bdo lang="EN-US">.+?<\/bdo>)/g, phonetic) // 英 <bdo>['dɪkt]</bdo>
+
+      if (symbol) {
+        symbol = symbol.map((v) => v.replace(/^(.)\s+/, '$1'))
+      }
+
+      audios = getRegValues(/naudio="(.+?)" title/g, phonetic)
+
+      if (audios) {
+        audios = audios.map((audioId) => `/audio/${audioId}`)
+      }
+    }
+
+    let dict = getRegValue(/<ul class="dict-basic-ul">([\s\S]+?)<\/ul>/, html)
+
+    if (dict) {
+      exp = getRegValues(/<li>\s+((?:<span>.+?<\/span>\s+)?<strong>.+?<\/strong>)\s+<\/li>/g, dict)
+
+      if (exp) {
+        exp = exp.map((v) => v.replace(/[\n\t]+/g, ' '))
+      }
+    } else {
+      dict = getRegValue(/<div class="layout cn">([\s\S]+?)<\/div>/, html)
+
+      exp = getRegValues(/<a href=".+?">\s+(\w+)\s+<\/a>/g, dict)
+    }
+
+    const result = { symbol, exp, audios, word, english: /^[a-z\-]+$/i.test(word) }
 
     if (exp) {
-      exp = exp.map((v) => v.replace(/[\n\t]+/g, ' '))
+      cache[key] = result
+      save2Cache()
     }
-  } else {
-    dict = getRegValue(/<div class="layout cn">([\s\S]+?)<\/div>/, html)
 
-    exp = getRegValues(/<a href=".+?">\s+(\w+)\s+<\/a>/g, dict)
-  }
-
-  return { audios, exp, symbol, word, english: /^[a-z\-]+$/i.test(word) }
-}, (e) => {
-  console.error(e)
-  return {}
-})
+    return result
+  }, (e) => {
+    console.error(`query word: [${word}] error`, e)
+    return {}
+  })
+}
 
 app.get('/dict', function (req, res) {
   let { word } = req.query
